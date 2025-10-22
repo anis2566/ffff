e6a352650961a714966ad42d18c83915969a2f66
Basic <anis2566>
2025-10-22 15:15:22 +0600

    fafa

--- apps/web/app/api/stream/webhook/route.ts
+++ apps/web/app/api/stream/webhook/route.ts
@@ -34,16 +34,19 @@ export async function POST(req: NextRequest) {
 
       if (chatUsers.length > 0) {
         for (const user of chatUsers) {
-          void triggerNotification({
-              identifier: "new_message",
-              recipients: [user.user.id],
-              data: {
-                messageSender: event.members.find(member => member.user.id === event.message.user.id)?.user.name,
-                message: event.message.text,
-                actionUrl: "/chat",
-                isOnline: event.members.find(member => member.user.id === event.message.user.id)?.user.online.toString(),
-              }
-          })
+            console.log("Message Sender", event.members.find(member => member.user.id === event.message.user.id)?.user.name)
+            console.log("Message", event.message.text)
+            console.log("is Online", event.members.find(member => member.user.id === event.message.user.id)?.user.online.toString())
+        //   void triggerNotification({
+        //       identifier: "new_message",
+        //       recipients: [user.user.id],
+        //       data: {
+        //         messageSender: event.members.find(member => member.user.id === event.message.user.id)?.user.name,
+        //         message: event.message.text,
+        //         actionUrl: "/chat",
+        //         isOnline: event.members.find(member => member.user.id === event.message.user.id)?.user.online.toString(),
+        //       }
+        //   })
         }
       }
     }
